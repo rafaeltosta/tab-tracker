289caad58f3060e052f9c9d13b47811e5b676336
Rafael Tosta <rafaeltosta@gmail.com>
2021-04-12 01:22:37 -0300

    basic full stack app

---
+++ client/src/components/Register.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+  <h1>Register</h1>
+    <input 
+    type="email"
+    name="email"
+    v-model="email"
+    placeholder="email"
+    /> 
+    <br>
+    <input
+    type="password"
+    name="password"
+    v-model="password"
+    placeholder="password"
+     /> 
+    <br>
+    <button @click='register'>Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+  data () {
+   // name: 'Register'
+   return {
+     email: '',
+     password:''
+   }
+  },
+  methods: {
+    async register () {
+      // console.log('register bt hein', this.email, this.password)
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+     }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
